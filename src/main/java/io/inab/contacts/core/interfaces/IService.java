e539f2251ba34494f4616f5c125a64905914a9cb
lapc18 <lapc18@outlook.com>
2023-02-11 18:34:06 -0400

    feat: add service interface and page response model

--- src/main/java/io/inab/contacts/core/interfaces/IService.java
+++ src/main/java/io/inab/contacts/core/interfaces/IService.java
@@ -1,2 +1,14 @@
-package io.inab.contacts.core.interfaces;public interface IService {
+package io.inab.contacts.core.interfaces;
+
+import io.inab.contacts.core.models.PageResponse;
+import org.springframework.data.domain.Pageable;
+
+public interface IService<T> {
+    T create(T dto) throws Exception;
+    T update(T dto) throws Exception;
+    void softDelete(int id) throws Exception;
+    void hardDelete(int id) throws Exception;
+    PageResponse<T> getAll(Pageable pageable) throws Exception;
+    T getById(int id) throws Exception;
 }
+
--- src/main/java/io/inab/contacts/core/models/PageResponse.java
+++ src/main/java/io/inab/contacts/core/models/PageResponse.java
@@ -1,2 +1,26 @@
-package io.inab.contacts.core.models;public class PageResponse {
+package io.inab.contacts.core.models;
+
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public class PageResponse<T> {
+    public List<T> body;
+    public int currentPage;
+    public long totalItems;
+    public int totalPages;
+
+    public PageResponse(Page<T> content) {
+        this.body = content.getContent();
+        this.currentPage = content.getNumber();
+        this.totalItems = content.getTotalElements();
+        this.totalPages = content.getTotalPages();
+    }
+
+    public PageResponse(List<T> body, int currentPage, long totalItems, int totalPages) {
+        this.body = body;
+        this.currentPage = currentPage;
+        this.totalItems = totalItems;
+        this.totalPages = totalPages;
+    }
 }
