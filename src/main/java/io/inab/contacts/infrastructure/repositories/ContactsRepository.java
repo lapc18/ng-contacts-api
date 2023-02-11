8866cf5eef3ca0f123c0297e5ad20e78b0f38a7a
lapc18 <lapc18@outlook.com>
2023-02-11 18:35:05 -0400

    feat: add initial repos and user service impl

--- src/main/java/io/inab/contacts/infrastructure/repositories/ContactsRepository.java
+++ src/main/java/io/inab/contacts/infrastructure/repositories/ContactsRepository.java
@@ -1,2 +1,14 @@
-package io.inab.contacts.infrastructure.repositories;public interface ContactsRepository {
+package io.inab.contacts.infrastructure.repositories;
+
+import io.inab.contacts.domain.entities.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ContactsRepository extends JpaRepository<Contact, Integer> {
+    List<Contact> findByFirstName(String firstName);
+    List<Contact> findByLastName(String lastName);
+    List<Contact> findByEmail(String email);
+    List<Contact> findByCompany(String company);
+    List<Contact> findByNickname(String nickname);
 }
--- src/main/java/io/inab/contacts/infrastructure/repositories/UsersRepository.java
+++ src/main/java/io/inab/contacts/infrastructure/repositories/UsersRepository.java
@@ -1,2 +1,10 @@
-package io.inab.contacts.infrastructure.repositories;public interface UsersRepository {
+package io.inab.contacts.infrastructure.repositories;
+
+import io.inab.contacts.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<User, Integer> {
+
+    User findByEmail(String email);
+
 }
